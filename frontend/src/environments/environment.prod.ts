11f3e3f0b811bcf7aba86f9569c0b0e5d9ff7723
Bat0al-k <batoalkaram0@gmail.com>
2025-07-02 02:23:38 +0300

    chore: added environment

---
+++ frontend/src/environments/environment.prod.ts
@@ -0,0 +1,14 @@
+export const environment = {
+    production: true,
+    apiUrl: 'https://fashion-platform-khaki.vercel.app/', // our server in production time 
+    STRIPE_PUBLIC_KEY:"pk_test_51ReeiRCc3gJwu9juMoTpuAL4jJprQFkhnIhv21NyUhkivXJIpN9WrEGeVfyj3s1QHyW14EQ0l4Dx8p24mghPfpBb00B3ZdK7kL"
+    ,firebaseConfig: {
+        apiKey: "AIzaSyDl4cTC6-LXjyXXSt1__jAeX6EQOpIHIzs",
+        authDomain: "fashion-platform-c82b9.firebaseapp.com",
+        projectId: "fashion-platform-c82b9",
+        storageBucket: "fashion-platform-c82b9.appspot.com",
+        messagingSenderId: "573354982996",
+        appId: "1:573354982996:web:e4eb4de94b1820843a37c1",
+        measurementId: "G-9EB3NP94JT"
+    }
+};
---
+++ frontend/src/environments/environment.ts
@@ -0,0 +1,50 @@
+export const environment = {
+    production: false,
+    apiUrl: 'http://localhost:3000/api/', // backend server in development time, i guess
+    // STRIPE_PUBLIC_KEY: 'pk_test_51RYWcQH6GDcoqFCbB8kdg9gOinlivCMIQ33MA7EVk1n7UmWtF5lAjvYK8goxiB9mktucvkfK0dUCgcWIotp6mYAc00dDa1S6F7',
+    STRIPE_PUBLIC_KEY:"pk_test_51ReeiRCc3gJwu9juMoTpuAL4jJprQFkhnIhv21NyUhkivXJIpN9WrEGeVfyj3s1QHyW14EQ0l4Dx8p24mghPfpBb00B3ZdK7kL"
+    ,firebaseConfig: {
+        apiKey: "AIzaSyDl4cTC6-LXjyXXSt1__jAeX6EQOpIHIzs",
+        authDomain: "fashion-platform-c82b9.firebaseapp.com",
+        projectId: "fashion-platform-c82b9",
+        storageBucket: "fashion-platform-c82b9.appspot.com",
+        messagingSenderId: "573354982996",
+        appId: "1:573354982996:web:e4eb4de94b1820843a37c1",
+        measurementId: "G-9EB3NP94JT"
+    }
+};
+
+// http://localhost:3000/api/users/login 
+
+
+
+
+
+
+
+
+
+// __________________________________________
+//  fire base signed by google
+
+// Import the functions you need from the SDKs you need
+// import { initializeApp } from "firebase/app";
+// import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// const firebaseConfig = {
+//   apiKey: "AIzaSyDl4cTC6-LXjyXXSt1__jAeX6EQOpIHIzs",
+//   authDomain: "fashion-platform-c82b9.firebaseapp.com",
+//   projectId: "fashion-platform-c82b9",
+//   storageBucket: "fashion-platform-c82b9.firebasestorage.app",
+//   messagingSenderId: "573354982996",
+//   appId: "1:573354982996:web:e4eb4de94b1820843a37c1",
+//   measurementId: "G-9EB3NP94JT"
+// };
+
+// // Initialize Firebase
+// const app = initializeApp(firebaseConfig);
+// const analytics = getAnalytics(app);
