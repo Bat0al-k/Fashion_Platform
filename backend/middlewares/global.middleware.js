864f4d4e5bfece629c5c2c3abda0e240c8f8f5b4
Bat0al-k <batoalkaram0@gmail.com>
2025-10-18 23:29:44 +0300

    fix(cors): allow frontend server URL in backend CORS configuration"

--- backend/middlewares/global.middleware.js
+++ backend/middlewares/global.middleware.js
@@ -6,16 +6,28 @@ const morgan = require('morgan');
 const cookieParser = require('cookie-parser');
 dotenv.config();
 
+const allowedOrigins = [
+    'http://localhost:4200',
+    'https://fashion-three-nu.vercel.app/',
+];
+
 module.exports = [
     express.json(),
     express.urlencoded(),
     cookieParser(),
     cors(
         {
-        origin: ["http://localhost:4200", "https://fashion-three-nu.vercel.app/"],    
+        origin: function (origin, callback) {
+            // allow requests with no origin 
+            if (!origin) return callback(null, true);
+            if (allowedOrigins.indexOf(origin) === -1) {
+                const msg = 'The CORS policy for this site does not allow access from the specified Origin.';
+                return callback(new Error(msg), false);
+            }
+            return callback(null, true);
+            },
         credentials: true
         }
     ),
     morgan('dev'),
 ]
-
